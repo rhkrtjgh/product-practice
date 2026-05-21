ae51254188cb42bf6c90d5438f35d5745fdef839
kwakbookair <ksh@kwakbookairui-MacBookAir.local>
2026-05-21 20:19:24 +0900

    스토리북 설정 파일 수정

--- market/practice-market/.storybook/main.ts
+++ market/practice-market/.storybook/main.ts
@@ -1,24 +1,30 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
-
-const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-mcp"
-  ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
-};
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  staticDirs: ['public']
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.mdx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss'
+  ],
+  staticDirs: ['public'],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ];
+    return config;
+  }
 }
-export default config;
--- market/practice-market/.storybook/preview.tsx
+++ market/practice-market/.storybook/preview.tsx
@@ -1,4 +1,55 @@
-import type { Preview } from '@storybook/nextjs-vite'
+import type { Preview } from '@storybook/nextjs-vite';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../src/themes';
+import * as NextImage from 'next/image';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls:{
+    matchers:{
+      color: /(background| color)$/i,
+      date: /Date$/,
+    }
+  }
+}
+
+export const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+padding: 0;
+margin: 0;
+font-family: -apple-system, BlinkMacSystemFont, Segoue UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neuem sans-serif;
+}
+* {
+box-sizing: border-box;
+}
+a {
+text-decoration: none;
+transition: .25s;
+color: #000000;
+}
+`
+
+//Theme적용
+addDecorator((Story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+));
+
+//next/image 교체
+const OriginalnextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalnextImage {...props} unoptimized blurDataURL={props.src}/>
+  ): (
+    <OriginalnextImage {...props} unoptimized />
+  )
+});
 
 const preview: Preview = {
   parameters: {
