53220f6f9988bc42451973b08a8070a62cb926f9
kwakbookair <ksh@kwakbookairui-MacBookAir.local>
2026-06-06 16:13:23 +0900

    organisms 구현

---
+++ market/practice-market/src/components/organisms/Header/index.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentMeta } from '@storybook/react'
+import React, { useEffect } from 'react'
+import Header from './index'
+import { AuthContextProvider } from 'contexts/AuthContext'
+import {
+  ShoppingCartContextProvider,
+  useShoppingCartContext,
+} from 'contexts/ShoppingCartContext'
+
+export default { title: 'organisms/Header' } as ComponentMeta<typeof Header>
+
+export const NoLogin = () => <Header />
+
+export const Login = () => {
+  const authUser = {
+    id: 1,
+    username: 'dummy',
+    displayName: 'Hana Kim',
+    email: 'hana.kim@example.com',
+    profileImageUrl: '/images/sample/1.jpg',
+    description: '',
+  }
+
+  const ChildComponent = () => {
+    const { addProductToCart } = useShoppingCartContext()
+
+    useEffect(() => {
+      addProductToCart({
+        id: 1,
+        category: 'book',
+        title: 'Product',
+        description: '',
+        imageUrl: '/images/sample/1.jpg',
+        blurDataUrl: '',
+        price: 10000,
+        condition: 'used',
+        owner: authUser,
+      })
+    }, [])
+    
+    return <Header />
+  }
+
+  return (
+    <ShoppingCartContextProvider>
+      <AuthContextProvider
+        context={{ apiRootUrl: 'https://dummy' }}
+        authUser={authUser}
+      >
+        <ChildComponent />
+      </AuthContextProvider>
+    </ShoppingCartContextProvider>
+  )
+}
